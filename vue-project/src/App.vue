4f18a0e0ce9b3ba97911cbd78d1329dbe52a434c
lujinzhong <32860251+lujinzhong@users.noreply.github.com>
2018-04-26 22:03:44 +0800

    慕课vue入门教程源代码

---
+++ vue-project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- 要求用一个标签包住所有的模板，否则会报错 -->
+  <div>
+      <input v-model="value"/>
+      <button @click="add">submit</button>
+      <div>
+        <ul>
+         <list v-for="(item,index) of list" :key="index" :content="item"
+          :index="index"  @delete="ondelete"></list>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+//引入组件
+import todolist from './components/todolist'
+
+export default {
+          components:{
+            //引用组件别名
+            'list':todolist
+
+          },
+          data(){
+            return{
+              value:"",
+              list:[]
+            }
+          },
+          methods:{
+                add:function()
+                {
+                  this.list.push(this.value),
+                  this.value=""
+                },
+               ondelete:function(index)
+                {
+
+                  this.list.splice(index)
+                }
+        }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-project/src/components/todolist.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+     <li @click='tough'>
+        {{content}}
+        </li>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    props:['content','index'],
+      methods:{
+        tough:function(){
+          //子组件向父组件传递消息
+          this.$emit('delete',this.index)
+          
+        }
+      }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
